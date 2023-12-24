b2cb9cfdee68136b41255e048564125dbffb4dd4
PxlSyl <SMCBeatz@protonmail.com.com>
2023-12-24 13:49:55 +0100

    init

--- components/navigation/lang/LangSwitch.tsx
+++ components/navigation/lang/LangSwitch.tsx
@@ -4,17 +4,25 @@ import { useOuterClick } from '@/hooks/useOuterclick'
 import { LocaleTypes, locales } from 'app/[locale]/i18n/settings'
 import { allBlogs } from '.contentlayer/generated'
 import slugMap from 'config/data/localeid-map.json'
-import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const LangSwitch = () => {
   const pathname = usePathname()
   const urlSegments = useSelectedLayoutSegments()
   const locale = useParams()?.locale as LocaleTypes
+  const router = useRouter()
 
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const handleLocaleChange = (newLocale: string): string => {
     const newUrl = `/${newLocale}/${urlSegments.join('/')}`
+
+    if (
+      newUrl.startsWith(`/${newLocale}/categories/`) ||
+      newUrl.startsWith(`/${newLocale}/tags/`)
+    ) {
+      return `/${newLocale}/${urlSegments.slice(0, -1).join('/')}`
+    }
 
     const currentPost = allBlogs.find((p) => pathname.includes(p.slug))
 
@@ -31,6 +39,16 @@ const LangSwitch = () => {
     return newUrl
   }
 
+  const handleLinkClick = async (newLocale: string) => {
+    try {
+      const resolvedUrl = await handleLocaleChange(newLocale)
+      router.push(resolvedUrl)
+    } catch (error) {
+      console.error('Error handling locale change:', error)
+      // Handle error as needed
+    }
+  }
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
   }
@@ -38,6 +56,7 @@ const LangSwitch = () => {
   const closeMenu = () => {
     setIsMenuOpen(false)
   }
+
   const menubarRef = useRef<HTMLDivElement>(null)
   useOuterClick(menubarRef, closeMenu)
 
@@ -66,16 +85,14 @@ const LangSwitch = () => {
           <ul className="py-1" role="none" style={{ listStyle: 'none', margin: 0, padding: 0 }}>
             {locales.map((newLocale: string) => (
               <li key={newLocale}>
-                <Link href={handleLocaleChange(newLocale)} locale={false}>
-                  <button
-                    className="px-4 py-2 text-sm text-gray-700  hover:bg-gray-100 dark:bg-[#3d3d3d] dark:text-white dark:hover:bg-[#4e4e4e]"
-                    role="menuitem"
-                    onClick={closeMenu}
-                    style={{ display: 'block', width: '100%', textDecoration: 'none' }}
-                  >
-                    {newLocale}
-                  </button>
-                </Link>
+                <button
+                  onClick={() => handleLinkClick(newLocale)}
+                  className="px-4 py-2 text-sm text-gray-700  hover:bg-gray-100 dark:bg-[#3d3d3d] dark:text-white dark:hover:bg-[#4e4e4e]"
+                  role="menuitem"
+                  style={{ display: 'block', width: '100%', textDecoration: 'none' }}
+                >
+                  {newLocale}
+                </button>
               </li>
             ))}
           </ul>
