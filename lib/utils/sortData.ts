36bdfde6c66d7cc7d91c404a8cd40b21e88c6a7a
PxlSyl <SMCBeatz@protonmail.com.com>
2024-01-18 18:06:18 +0100

    fix sortData

--- lib/utils/sortData.ts
+++ lib/utils/sortData.ts
@@ -1,4 +1,4 @@
 export const sortData = (data: Record<string, number>) => {
-  const keys = Object.keys(data)
-  return keys.sort((a, b) => data[b] - data[a])
+  const keys = Object.keys(data ?? {})
+  return keys.sort((a, b) => a.localeCompare(b))
 }
